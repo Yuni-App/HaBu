16dc0582f483c4c8f4d654451b2516e8ea58efc3
Omer Erbalta <erbalta80@gmail.com>
2023-11-14 17:01:16 +0300

    created project

---
+++ HaBu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HaBu
+//
+//  Created by OmerErbalta on 14.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HaBu/HaBuApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaBuApp.swift
+//  HaBu
+//
+//  Created by OmerErbalta on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct HaBuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
