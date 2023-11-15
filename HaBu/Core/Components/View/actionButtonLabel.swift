144aff51724ce3470e5bf03cfda7dc170fe7f521
Omer Erbalta <erbalta80@gmail.com>
2023-11-15 16:23:13 +0300

    created FeedViewCell

---
+++ HaBu/Core/Components/View/actionButtonLabel.swift
@@ -0,0 +1,18 @@
+//
+//  actionButtonLabel.swift
+//  HaBu
+//
+//  Created by OmerErbalta on 15.11.2023.
+//
+
+import SwiftUI
+
+struct actionButtonLabel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    actionButtonLabel()
+}
---
+++ HaBu/Core/Feed/FeedViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewCell.swift
+//  HaBu
+//
+//  Created by OmerErbalta on 15.11.2023.
+//
+
+import SwiftUI
+
+struct FeedViewCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedViewCell()
+}
