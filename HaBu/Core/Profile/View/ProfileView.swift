743e51198411670f742d4ace30d234ce3a6fc5cf
Yusuf Aydın <yusayd3825@gmail.com>
2023-11-15 19:41:15 +0300

    edited edit button

--- HaBu/Core/Profile/View/ProfileView.swift
+++ HaBu/Core/Profile/View/ProfileView.swift
@@ -8,6 +8,26 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @GestureState private var isDetectingLongPress = false
+    @State private var completedLongPress = false
+    @State private var offset: CGSize = .zero
+    
+    var longPressGesture: some Gesture {
+        LongPressGesture(minimumDuration: 1)
+            .updating($isDetectingLongPress) { currentState, _, _ in
+                self.completedLongPress = currentState
+            }
+            .onChanged { value in
+                self.offset.width = -10 // Sola kaydırmak için negatif bir değer kullanın
+            }
+            .onEnded { _ in
+                // Burada gerekirse long press sona erdiğinde yapılacak işlemleri ekleyebilirsiniz.
+            }
+    }
+    
+    
+    
     var body: some View {
         VStack{
             
@@ -32,9 +52,10 @@ struct ProfileView: View {
                     Spacer()
                     Text("4 Post").frame(width: 75, height: 20).padding(.vertical, 4).padding(.horizontal, 8)
                     Spacer()
-                    Button {
+                    Button(action: {
+                        //Goes to profile editing page based on button
                         print("tapped")
-                    } label: {
+                    }) {
                         Spacer()
                         Text("< Edit").font(.headline).foregroundColor(.black)
                             .padding(.horizontal, 20)
@@ -48,6 +69,8 @@ struct ProfileView: View {
                                     topTrailingRadius: 0
                                 )
                             )
+                            .offset(offset)
+                            .gesture(longPressGesture)
                     }.frame(width: 100, height: 55)
                 }
                 
