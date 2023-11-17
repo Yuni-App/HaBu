f8b613fa16c911e0d3f566787c0c2029d9429bd0
Yusuf Aydın <yusayd3825@gmail.com>
2023-11-17 13:59:02 +0300

    bug fix

--- HaBu/Core/Authentication/LoginView.swift
+++ HaBu/Core/Authentication/LoginView.swift
@@ -8,72 +8,84 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var showingForgotPassword = false
+    @State private var goPage = false
     
     var body: some View {
         NavigationStack{
             ZStack {
                 VStack{
-                    CustomImageRectangle(width: 537, height: 376, imagePath: ImageManager.registerVector)
-                    CustomTextField(icon: "mail", placeHolder: "e-posta")
-                    CustomTextField(icon: "key", placeHolder: "Şifre")
-                    HStack{
-                        Spacer()
-                        Text("Şifremi unuttum.")
-                        
-                            .padding()
-                            .onTapGesture {
-                                //  Bottomsheet will openSheet
-                                print("Şifremi unuttum ")
+                    CustomImageRectangle(width: Const.width, height: Const.height/2, imagePath: ImageManager.loginVector)
+                    ZStack{
+                        Image("")
+                            .frame(width: 370, height: 260)
+                            .background(Color(red: 0.65, green: 0.65, blue: 0.65).opacity(0.1))
+                            .cornerRadius(10)
+                            .shadow(color: Color.black.opacity(1), radius: 5, x: 4, y:4)
+                        VStack{
+                            CustomTextField(icon: "mail", placeHolder: "e-posta")
+                            CustomTextField(icon: "key", placeHolder: "Şifre")
+                            HStack{
+                                Spacer()
+                                Text("Şifremi unuttum.")
+                                    .padding()
+                                    .onTapGesture {
+                                        showingForgotPassword = true
+                                        //  Bottomsheet will openSheet
+                                        print("Şifremi unuttum ")
+                                    }
+                                    .sheet(isPresented: $showingForgotPassword) {
+                                        ForgotPasswordMailBottomSheet(showSheet: $showingForgotPassword , goPage: $goPage)
+                                            .presentationDetents([.medium,.height(CGFloat(Const.height/4 + 10))])
+                                    }
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 12))
                             }
-                            .foregroundColor(.white)
-                            .font(.system(size: 14))
-                    }
-                    HStack{
-                        Spacer()
-                        CustomButton(title: "Giriş Yap", backgroundColor: Const.primaryColor, action: {
-                            //will delete the back button from feedview
-                            print("cliked login button")
-                        }, destinaiton: AnyView(FeedView()),width: Const.width/3)
-                    }
-                    Spacer()
-                    HStack{
-                        Text("Bir hesabınız yok mu ?").foregroundStyle(.black).font(.system(size: 14))
-                        NavigationLink {
-                            RegisterBuildFirstView()
-                        } label: {
-                            HStack{  Text("Kayıt Ol").foregroundStyle(.blue).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)}
+                            
+                            CustomButton(title: "Giriş Yap", backgroundColor: Const.primaryColor, action: {
+                                //will delete the back button from feedview
+                                print("cliked login button")
+                            }, destinaiton: AnyView(TabbarView())).padding(.vertical , 30)
                         }
-                        .foregroundColor(.gray)
-                        .font(.system(size: 14))
                     }
                     HStack{
-                        Spacer()
-                        CustomButton(title: "Giriş Yap", backgroundColor: Const.primaryColor, action: {
-                            //will delete the back button from feedview
-                            print("cliked login button")
-                        }, destinaiton: AnyView(TabbarView()),width: Const.width/3)
-                    }
-                    Spacer()
-                    HStack{
-                        Text("Bir hesabınız yok mu ?").foregroundStyle(.black).font(.system(size: 14))
+                        Text("Bir hesabınız yok mı?").foregroundStyle(.black).font(.system(size: 14))
                         NavigationLink {
                             RegisterBuildFirstView()
                         } label: {
                             HStack{  Text("Kayıt Ol").foregroundStyle(.blue).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)}
                         }
                     }
-                    .padding(30)
-                    .frame(width: 430, height: 932)
+                }.padding()
+                    .frame(width: Const.width, height: 1000)
                     .background(
                         BackgroundLinearColor()
                     )
-                }
-            }
+            }.background(
+                NavigationLink("", destination: TabbarView(), isActive: $goPage).hidden()
+            )
+            .foregroundColor(.gray)
+            .font(.system(size: 14))
         }
+        HStack{
+            Spacer()
+            CustomButton(title: "Giriş Yap", backgroundColor: Const.primaryColor, action: {
+                //will delete the back button from feedview
+                print("cliked login button")
+            }, destinaiton: AnyView(TabbarView()),width: Const.width/3)
+        }  .padding(30)
+            .frame(width: 430, height: 932)
+            .background(
+                BackgroundLinearColor()
+            )
     }
-}
     
-#Preview {
-        LoginView()
-    }
+}
 
+
+
+
+
+#Preview {
+    LoginView()
+}
