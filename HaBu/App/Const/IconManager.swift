39b91a4385c64e4718a285779413bc8d63abb501
Omer Erbalta <erbalta80@gmail.com>
2023-11-23 15:13:54 +0300

    AppIcon Extencion

--- HaBu/App/Const/IconManager.swift
+++ HaBu/App/Const/IconManager.swift
@@ -8,29 +8,14 @@
 import Foundation
 import SwiftUI
 
-enum AppIcon: String, CaseIterable {
+
+enum AppIcon: String{
     case mail = "mail"
-
-    //ikonlar buraya eklenmeli
-    var systemName: String {
-        return "systemName: \(rawValue)"
-    }
+    case key = "key"
+    case envelope = "envelope"
+    case person = "person.fill"
+    case personBadgePlus = "person.badge.plus"
+    case calendar = "calendar"
+    case pencil = "pencil"
+    case lock = "lock.circle"
 }
-
-class IconManager {
-    static let shared = IconManager()
-    
-    private init() {}
-    func getIcon(_ icon: AppIcon, size: CGFloat = 24, weight: Font.Weight = .regular, color: Color = .primary) -> some View {
-        return Image(systemName: "mail")
-            .resizable()
-            .frame(width: size, height: size)
-            .font(Font.system(size: size, weight: weight))
-            .foregroundColor(color)
-    }
-}
-//kullanımı
-// enum yapısına iconunuzu ekleyin
-//view içinde IconManager.shared.getIcon(.mail, size: 50, weight: .bold, color: .blue)
-//şeklinde kullanaiblirsiniz
-//icon zorunludur diğer seçenekler opsiyoneldir
