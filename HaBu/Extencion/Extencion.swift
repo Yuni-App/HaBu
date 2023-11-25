35c16f09ef38af27bcb47f9d4f5173812faff245
Omer Erbalta <erbalta80@gmail.com>
2023-11-25 23:16:44 +0300

    [LayoutSubview.Elements] extencion

--- HaBu/Extencion/Extencion.swift
+++ HaBu/Extencion/Extencion.swift
@@ -69,5 +69,11 @@ extension Image {
             .scaledToFit()
     }
 }
-
+extension [LayoutSubviews.Element]{
+    func maxHeight(_ proposal: ProposedViewSize) -> CGFloat{
+        return self.compactMap { view in
+            return view.sizeThatFits(proposal).height
+        }.max() ?? 0
+    }
+}
 
