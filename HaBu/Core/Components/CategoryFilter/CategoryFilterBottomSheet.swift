49bdfbe396cc5b9afc3a84c4264880a28ee74abf
Omer Erbalta <erbalta80@gmail.com>
2023-11-25 23:14:45 +0300

    created CategoryFilterBottomSheet

---
+++ HaBu/Core/Components/CategoryFilter/CategoryFilterBottomSheet.swift
@@ -0,0 +1,102 @@
+//
+//  CategoryFilterBottomSheet.swift
+//  HaBu
+//
+//  Created by OmerErbalta on 25.11.2023.
+//
+
+import SwiftUI
+
+struct CategoryFilterBottomSheet: View {
+    let tags = ["ömer", "mert","yusuf","kerem","burak","selim","yiğit","ismail"]
+    @State var SelectedTags:[String] = []
+@Namespace private var animation
+    var body: some View {
+        VStack {
+            ScrollView(.horizontal) {
+                HStack(spacing: 10){
+                    ForEach(SelectedTags ,id: \.self){tag in
+                        TagView(tag, .pink, "checkmark")
+                            .matchedGeometryEffect(id: tag, in: animation)
+                            .onTapGesture {
+                                withAnimation(.snappy){
+                                    SelectedTags.removeAll(where: {$0 == tag})
+                                }
+                            }
+                            
+                    }
+                }
+                .padding(.horizontal,15)
+                .frame(height: 35)
+                .padding(.vertical,15)
+            }
+            .scrollClipDisabled(true)
+            .scrollIndicators(.hidden)
+            .overlay {
+                if SelectedTags.isEmpty{
+                    Text("Şeçilen Kategoriler")
+                        .font(.callout)
+                        .foregroundStyle(.gray)
+                    
+                }
+            }
+            .background(.white)
+            .zIndex(1)
+            ScrollView(.vertical){
+                TagLayout(spacing: 10){
+                    ForEach(tags.filter{!SelectedTags.contains($0)} , id: \.self){tag in
+                        TagView(tag, .blue, "plus")
+                            .matchedGeometryEffect(id: tag, in: animation)
+                            .onTapGesture {
+                                withAnimation(.snappy){
+                                    SelectedTags.insert(tag, at: 0)
+                                }
+                            }
+                    }
+                }
+                .padding(15)
+            }
+            .scrollClipDisabled(true)
+            .scrollIndicators(.hidden)
+            .background(.black.opacity(0.07))
+            .zIndex(0)
+            ZStack{
+                Button(action: {}, label: {
+                    Text("Continue")
+                        .fontWeight(.semibold)
+                        .padding(.vertical,15)
+                        .frame(maxWidth: .infinity)
+                        .background(RoundedRectangle(cornerRadius: 12)
+                            .fill(.pink.gradient)
+                        )
+                    
+                })
+                .disabled(SelectedTags.count < 3)
+                .opacity(SelectedTags.count < 3 ? 0.5 : 1)
+                .padding()
+                .foregroundStyle(.white)
+                .background(.white)
+                .zIndex(2)
+            }
+        }
+}
+}
+
+#Preview {
+    CategoryFilterBottomSheet()
+}
+@ViewBuilder
+func TagView(_ tag:String ,_ color: Color, _ icon:String)->some View{
+    HStack(spacing:10){
+        Text(tag)
+            .font(.callout)
+            .fontWeight(.semibold)
+        Image(systemName:icon)
+    }
+    .frame(height: 35)
+    .foregroundStyle(.white)
+    .padding(.horizontal,15)
+    .background(
+        Capsule().fill(color.gradient)
+    )
+}
---
+++ HaBu/Core/Components/CategoryFilter/TagLayout.swift
@@ -0,0 +1,21 @@
+//
+//  TagLayout.swift
+//  HaBu
+//
+//  Created by OmerErbalta on 25.11.2023.
+//
+
+import SwiftUI
+
+struct TagLayout: Layout {
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        <#code#>
+    }
+    
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        <#code#>
+    }
+    
+    
+}
+
