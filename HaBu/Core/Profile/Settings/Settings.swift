b5746001df95e7aeceb88890d998a2bfdb0c04f6
Yusuf Aydın <yusayd3825@gmail.com>
2023-12-01 17:33:28 +0300

    update searchView and created searchItem

---
+++ HaBu/Core/Profile/Settings/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  HaBu
+//
+//  Created by yusuf on 1.12.2023.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
--- HaBu/Core/Serach/View/SerachView.swift
+++ HaBu/Core/Serach/View/SerachView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+
+
 struct SerachView: View {
     @State private var searchText = ""
     @State private var isSearchBar = false
@@ -36,70 +38,15 @@ struct SerachView: View {
         }
     }
     var body: some View {
-        NavigationStack{
-            
-            ScrollView{
-                LazyVStack(spacing:15){
-                    //User.MockData = filtereduser
-                    ForEach(filteredUser, id : \.id){user in
-                        NavigationLink(destination: Text("ProfileView"),
-                                       label: {
-                            HStack(){
-                                CircleProfileImage(userIamgeUrl: "", size: .small).frame(width: Const.width * 0.15)
-                                VStack (alignment:.leading){
-                                    Text("\(user.name) \(user.surName)")
-                                        .opacity(0.85)
-                                        .font(.title3)
-                                    Text(user.username).opacity(0.6).font(.footnote)
-                                    
-                                }
-                                Spacer()
-                                
-                                HStack {
-                                    switch (ratingSorted, user.rating) {
-                                    case (false, 20...30):
-                                        Image(systemName:"star.fill")
-                                            .foregroundColor(Color(UIColor(hex: "#CD7F32")))
-                                    case(false, 30...40):
-                                        Image(systemName:"star.fill")
-                                            .foregroundColor(Color(UIColor(hex: "#808080")))
-                                    case (false, 40...50):
-                                        
-                                        Image(systemName: "star.fill").foregroundStyle(Color(UIColor(hex: "#FFD700"))
-                                        )
-                                        
-                                    case (false, 50...65):
-                                        Image(systemName: "star.fill").foregroundStyle(Color(UIColor(hex: "#FFD700"))
-                                        )
-                                        Image(systemName: "star.fill").resizable().frame(width: Const.width * 0.03, height: Const.height * 0.015).foregroundStyle(Color(UIColor(hex: "#FFD700")).secondary
-                                        )
-                                    case (false, 65...100):
-                                        Image(systemName: "star.fill").resizable().frame(width: Const.width * 0.03, height: Const.height * 0.015).foregroundStyle(Color(UIColor(hex: "#FFD700")).secondary
-                                        )
-                                        Image(systemName: "star.fill").foregroundStyle(Color(UIColor(hex: "#FFD700"))
-                                        )
-                                        Image(systemName: "star.fill").resizable().frame(width: Const.width * 0.03, height: Const.height * 0.015).foregroundStyle(Color(UIColor(hex: "#FFD700")).secondary
-                                        )
-                                        
-                                        
-                                    default:
-                                        Image(systemName: "star")
-                                            .foregroundColor(Color.gray)
-                                    }
-                                }.frame(width: Const.width * 0.2)
-                                
-                            }
-                            .frame(width: Const.width * 0.9, height:Const.height * 0.08, alignment: .leading)
-                            
-                            .background(Color(UIColor(hex: "#FFFBF1"))).cornerRadius(10)
-                            .shadow(color: .black.opacity(0.25), radius: 10)
-                            .foregroundStyle(.black)
-                            .padding(.leading,3)
-                        })
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 15) {
+                    ForEach(filteredUser, id: \.id) { user in
+                        SearchItem(user: user, ratingSorted: ratingSorted)
                     }
                 }
-                .padding(.top,15)
-                .searchable(text: $searchText,prompt: "Arama...").onChange(of: searchText) { oldValue, newValue in
+                .padding(.top, 15)
+                .searchable(text: $searchText, prompt: "Arama...").onChange(of: searchText) { oldValue, newValue in
                     isSearchBar = newValue != ""
                 }
             }
@@ -108,6 +55,8 @@ struct SerachView: View {
         }
     }
 }
+
+
 
 #Preview {
     SerachView()
