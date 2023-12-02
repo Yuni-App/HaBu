c585e576b7e35938b6b4d0370d1b6502407b7f3b
Yusuf Aydın <yusayd3825@gmail.com>
2023-12-02 16:37:32 +0300

    completed searchView1

---
+++ HaBu/Core/Serach/View/SecondSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondSearchView.swift
+//  HaBu
+//
+//  Created by yusuf on 2.12.2023.
+//
+
+import SwiftUI
+
+struct SecondSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondSearchView()
+}
--- HaBu/Core/Serach/View/SerachView.swift
+++ HaBu/Core/Serach/View/SerachView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 
-
 struct SerachView: View {
     @State private var searchText = ""
     @State private var isSearchBar = false
@@ -26,58 +25,69 @@ struct SerachView: View {
             return Array(sortedUsers.prefix(3))
         }
     }
+    var filteredUsers2: [User] {
+        let sortedUsers2 = User.MockData.filter { $0.rating > 0 }.sorted { $0.rating > $1.rating }
+        let startIndex = 3
+        let endIndex = min(sortedUsers2.count, 10)
+        return Array(sortedUsers2[startIndex..<endIndex])
+    }
     var body: some View {
         NavigationStack {
+            SearchBar(searchText: $searchText, isEditing: $isSearchBar)
+                .padding(.top, 8)
             ScrollView {
                 VStack {
-                    SearchBar(searchText: $searchText, isEditing: $isSearchBar)
-                                            .padding(.top, 8)
                     LazyVStack(spacing: 15) {
                         ForEach(filteredUsers, id: \.id) { user in
                             SearchItem(user: user, ratingSorted: ratingSorted)
                         }
                         .padding(.top,15)
-                        
-                        }
-                        .navigationTitle("Arama")
-                    .navigationBarTitleDisplayMode(.inline)
-                }
+                    }
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack{
+                            Spacer()
+                            ForEach(filteredUsers2, id: \.id) { user in
+                                SearchItem2(user: user, ratingSorted: ratingSorted)
+                            }
+                            .padding(.top,15)
+                            Spacer()
+                        }.frame(height: Const.height * 0.25)
+                    }
                 }
             }
         }
     }
+}
 struct SearchBar: View {
     @Binding var searchText: String
     @Binding var isEditing: Bool
-
+    
     var body: some View {
-        HStack {
-            TextField("Ara", text: $searchText)
-                .padding(8)
-                .padding(.horizontal, 24)
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
-                .onTapGesture {
-                    isEditing = true
-                }
-
-            if isEditing {
-                Button(action: {
-                    searchText = ""
-                    isEditing = false
-                }) {
-                    Image(systemName: "multiply.circle.fill")
-                        .foregroundColor(.gray)
-                        .padding(8)
+        NavigationStack {
+            HStack {
+                TextField("Ara...", text: $searchText)
+                    .padding(8)
+                    .padding(.horizontal, 24)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .onTapGesture {
+                        isEditing = true
+                    }
+                if isEditing {
+                    Button(action: {
+                        searchText = ""
+                        isEditing = false
+                    }) {
+                        Image(systemName: "multiply.circle.fill")
+                            .foregroundColor(.gray)
+                            .padding(8)
+                    }
                 }
             }
+            .padding(.horizontal)
         }
-        .padding(.horizontal)
     }
 }
-
-
-
 
 #Preview {
     SerachView()
