3f4c0fe9201170eddc41001b365b5384e246321a
Yusuf Aydın <yusayd3825@gmail.com>
2023-12-07 19:51:01 +0300

    created feedBackInput and success

---
+++ HaBu/Core/Settings/View/FeedBack/FeedBackSuccess.swift
@@ -0,0 +1,18 @@
+//
+//  FeedBackSuccess.swift
+//  HaBu
+//
+//  Created by yusuf on 6.12.2023.
+//
+
+import SwiftUI
+
+struct FeedBackSuccess: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedBackSuccess()
+}
