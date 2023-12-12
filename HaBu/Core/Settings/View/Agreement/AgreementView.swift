2f332d32e80014f7f7991d60f8c09bd5beccdfc0
mertalp <mertalp010@gmail.com>
2023-12-12 15:53:30 +0300

    update settings

--- HaBu/Core/Settings/View/Agreement/AgreementView.swift
+++ HaBu/Core/Settings/View/Agreement/AgreementView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct AgreementView: View {
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
         VStack{
-          CustomSettingsTollBar(title: "Gizlilik Sözleşmesi",destinaiton: AnyView(SettingsView()))
+            CustomSettingsTollBar(action: {
+                dismiss()
+            }, title: "Gizlilik Sözleşmesi")
+       
             ScrollView{
                 Text(Const.agreement).padding(25)
             }.frame(width: Const.width)
