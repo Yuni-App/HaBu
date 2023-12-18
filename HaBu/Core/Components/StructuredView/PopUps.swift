1899e2bb89184ce5e76cd5e84ae15b4a925a8606
Yusuf Aydın <yusayd3825@gmail.com>
2023-12-18 12:38:21 +0300

    update PopUps

--- HaBu/Core/Components/StructuredView/PopUps.swift
+++ HaBu/Core/Components/StructuredView/PopUps.swift
@@ -9,34 +9,33 @@ import SwiftUI
 
 class PopUps{
     @ViewBuilder
-    static func AddPostPopup(selectedOption : Binding<ImageType> , isPopupVisible : Binding<Bool>)->some View {
+    static func AddPostPopup(selectedOption : Binding<ImageType> , isPopupVisible : Binding<Bool>, title: String, contents: String, size: PopUpScreen,backgroundColor: Color)->some View {
         VStack {
-            Text("Gönderinizin Gizliliği Nasıl Olsun ? ").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.black).padding(.bottom,5)
-            Text("Anonim Gönderi Seçeneği ile gizli paylaşım yapıp istemediğiniz etkileşimlerden kaçabilirsiniz...").foregroundColor(Const.secondaryButtonColor)
+            Text(title).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.black).padding(.bottom,5)
             Spacer()
+            Text(contents).foregroundColor(Const.secondaryButtonColor)
             Spacer()
-            CustomButton(title:  "Anonim Gönderi", backgroundColor: Const.primaryButtonColor, action: {
-                withAnimation {
-                    selectedOption.wrappedValue = .anonymous
-                    isPopupVisible.wrappedValue.toggle()
-                }
-                return false
-            }, size: CustomButtonSize.medium , textColor: .white)
-            Spacer()
-            CustomButton(title:  "Açık Gönderi", backgroundColor: Const.primaryButtonColor.opacity(0.7), action: {
-                withAnimation {
-                    selectedOption.wrappedValue = .notAnonymous
-                    isPopupVisible.wrappedValue.toggle()
-                }
-                return false
-            }, size: CustomButtonSize.medium , textColor: .white)
-          Spacer()
+            HStack{
+                CustomButton(title:  "Anonim Gönderi", backgroundColor: Const.primaryButtonColor, action: {
+                    withAnimation {
+                        selectedOption.wrappedValue = .anonymous
+                        isPopupVisible.wrappedValue.toggle()
+                    }
+                    return false
+                }, size: CustomButtonSize.xxsmall , textColor: .white)
+                CustomButton(title:  "Açık Gönderi", backgroundColor: Const.primaryButtonColor.opacity(0.7), action: {
+                    withAnimation {
+                        selectedOption.wrappedValue = .notAnonymous
+                        isPopupVisible.wrappedValue.toggle()
+                    }
+                    return false
+                }, size: CustomButtonSize.xxsmall , textColor: .white)
+            }
         }.padding(.vertical,10)
         .padding(.horizontal,5)
-        .frame(width: Const.width * 0.85,height: Const.width * 0.6 )
-        .cornerRadius(15)
-        
-        
+        .frame(width: size.width ,height: size.height )
+        .background(backgroundColor)
+        .cornerRadius(5)
     }
 
 }
