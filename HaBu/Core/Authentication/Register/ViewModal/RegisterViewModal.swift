a95f6ff6d9ff4c9b2a1e3e07a782733fa6a64ea7
mertalp <mertalp010@gmail.com>
2023-12-20 12:49:49 +0300

    added registerVM

--- HaBu/Core/Authentication/Register/ViewModal/RegisterViewModal.swift
+++ HaBu/Core/Authentication/Register/ViewModal/RegisterViewModal.swift
@@ -10,13 +10,16 @@ import Foundation
 class RegisterViewModel: ObservableObject {
     @Published var textEmail = ""
     @Published var textPassword = ""
-    @Published var againTextPassword = ""
-    @Published var name = ""
-    @Published var sureName = ""
-    @Published var userName = ""
-    @Published var age = ""
-    @Published var bio = ""
-    
+    @Published var textAgainPassword = ""
+    @Published var textName = ""
+    @Published var textSurname = ""
+    @Published var textUserName = ""
+    @Published var textAge = ""
+    @Published var textBio = ""
+    @Published var textFaculty = ""
+    @Published var textDepartment = ""
+    @Published var textYear = ""
+
     @Published var errorMessage = ""
     @Published var completion = false
     @Published var error = false
@@ -27,10 +30,7 @@ class RegisterViewModel: ObservableObject {
        }
 
     
-    func signUp() {
-        print("sfdghj")
-        print(name)
-    }
+    func signUp() {}
     
 
        
