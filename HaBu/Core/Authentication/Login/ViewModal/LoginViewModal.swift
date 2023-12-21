d8319c4a9ece6f9697eaf902bfe6956814f0aecb
mertalp <mertalp010@gmail.com>
2023-12-21 15:34:09 +0300

    update auth vm

--- HaBu/Core/Authentication/Login/ViewModal/LoginViewModal.swift
+++ HaBu/Core/Authentication/Login/ViewModal/LoginViewModal.swift
@@ -8,6 +8,7 @@
 import Foundation
 import FirebaseAuth
 
+
 class LoginViewModel: ObservableObject {
     @Published var textEmail = ""
     @Published var textPassword = ""
@@ -18,21 +19,67 @@ class LoginViewModel: ObservableObject {
     @Published var error = false
     
     private var authService: AuthService
-       init(authService: AuthService) {
-           self.authService = authService
-       }
+    init(authService: AuthService) {
+        self.authService = authService
+    }
 
-    
-    func signIn() {
-        // E-posta ve şifre boş mu kontrolü
-        authService.signIn(email: textEmail, password: textPassword) {AuthResult in
-            self.error = !AuthResult.success
-            self.errorMessage = AuthResult.errorMessage
-            self.completion = AuthResult.success
+    enum ErrorMessage: Int  {
+        case emptyFields = 1
+        case invalidEmail = 2
+        case wrongPassword = 17009
+        case userNotFound = 4
+        case authenticationError = 5
+        
+        var description: String {
+            switch self {
+            case .emptyFields:
+                return "E-posta ve şifre alanları boş bırakılamaz."
+            case .invalidEmail:
+                return "Geçersiz e-posta adresi. Lütfen geçerli bir e-posta adresi girin."
+            case .wrongPassword:
+                return "Yanlış şifre. Lütfen doğru şifreyi girin."
+            case .userNotFound:
+                return "Kullanıcı bulunamadı. Lütfen kayıtlı bir e-posta adresi kullanın veya hesap oluşturun."
+            case .authenticationError:
+                return "Kimlik doğrulama hatası. Lütfen tekrar deneyin."
+            }
         }
     }
+
+    func signIn() {
     
-    func forgotPassoword(){
-      //  authService.forgotPassoword()
+         guard !textEmail.isEmpty, !textPassword.isEmpty else {
+             if let errorMessage = ErrorMessage(rawValue: 1) {
+                 print(errorMessage.description)
+                 
+             }
+         return
+         }
+         guard textEmail.isValidEmail else {
+             if let errorMessage = ErrorMessage(rawValue: 2) {
+                 print(errorMessage.description)
+                 
+             }
+         return
+         }
+        authService.signIn(email: textEmail, password: textPassword) { result in
+            switch result {
+            case .success(let result):
+                print(result)
+            case .failure(let error as NSError):
+                if let errorMessage = ErrorMessage(rawValue: error.code) {
+                    print(errorMessage.description)
+                    
+                } else {
+                    print("Bilinmeyen bir hata oluştu.")
+                }
+            }
+        }
+        // E-posta ve şifre boş mu kontrolü
     }
 }
+    
+    func forgotPassoword(){
+        //  authService.forgotPassoword()
+    }
+
--- HaBu/Service/AuthService.swift
+++ HaBu/Service/AuthService.swift
@@ -7,89 +7,58 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestore
 
 protocol AuthProtocol {
-    func signIn(email: String, password: String, completion: @escaping (AuthResult) -> Void)
+    func signIn(email: String, password: String, completion: @escaping (Result<AuthDataResult,Error>) -> Void)
     func forgotPassword()
-    func signUp(email: String, password: String, passwordAgain: String, completion: @escaping (AuthResult) -> Void)
+ //   func signUp(email: String, password: String, passwordAgain: String, completion: @escaping (AuthResult) -> Void)
     func logOut()
 }
 
-class AuthResult {
-    var success : Bool = false
-    var errorMessage : String = ""
-    init(success : Bool, errorMessage: ErrorMessage) {
-        self.success = success
-        self.errorMessage = errorMessage.description
-    }
-}
-
-enum ErrorMessage {
-    case emptyFields
-    case invalidEmail
-    case wrongPassword
-    case userNotFound
-    case authenticationError
-
-    var description: String {
-        switch self {
-        case .emptyFields:
-            return "E-posta ve şifre alanları boş bırakılamaz."
-        case .invalidEmail:
-            return "Geçersiz e-posta adresi. Lütfen geçerli bir e-posta adresi girin."
-        case .wrongPassword:
-            return "Yanlış şifre. Lütfen doğru şifreyi girin."
-        case .userNotFound:
-            return "Kullanıcı bulunamadı. Lütfen kayıtlı bir e-posta adresi kullanın veya hesap oluşturun."
-        case .authenticationError:
-            return "Kimlik doğrulama hatası. Lütfen tekrar deneyin."
-        }
-    }
-}
 
 
 
 class AuthService: AuthProtocol {
-
-    
-    func signIn(email: String, password: String, completion: @escaping (AuthResult) -> Void) {
-           Auth.auth().signIn(withEmail: email, password: password) { (res, error) in
-               
-               guard !email.isEmpty, !password.isEmpty else {
-                   completion(AuthResult(success: false, errorMessage: .emptyFields))
-                   return
-               }
-               // E-posta format kontrolü
-               guard email.isValidEmail else {
-                   completion(AuthResult(success: false, errorMessage: .invalidEmail))
-                   return
-               }
-
-               Auth.auth().signIn(withEmail: email, password: password) { result, error in
-                   if let error = error as? NSError {
-                       print("Authentication error: \(error.localizedDescription)")
-                       switch error.code {
-                       case AuthErrorCode.invalidEmail.rawValue:
-                           completion(AuthResult(success: false, errorMessage: .invalidEmail))
-                       case AuthErrorCode.wrongPassword.rawValue:
-                           completion(AuthResult(success: false, errorMessage: .wrongPassword))
-                       case AuthErrorCode.userNotFound.rawValue:
-                           completion(AuthResult(success: false, errorMessage: .userNotFound))
-                       default:
-                           completion(AuthResult(success: false, errorMessage: .authenticationError))
-                       }
-                   } else {
-                       print("Sign-in successful")
-                       //update
-                       completion(AuthResult(success: true, errorMessage: .authenticationError))
-                   }
-               }
-           }
-       }
+    func signIn(email: String, password: String, completion: @escaping (Result<AuthDataResult,Error>) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error as? NSError {
+                completion(.failure(error))
+            }
+           else if let result = result {
+               completion(.success(result))
+            }
+        }
+    }
+      
     
     func forgotPassword() {}
-  
-    func signUp(email: String, password: String,passwordAgain : String ,  completion: @escaping (AuthResult) -> Void){}
+   // func signUp(email: String, password: String,passwordAgain : String ,  completion: @escaping (AuthResult) -> Void){}
     func logOut(){}
-
 }
+
+/*
+ 
+ guard !email.isEmpty, !password.isEmpty else {
+     completion(AuthResult(success: false, errorMessage: .emptyFields))
+     return
+ }
+ // E-posta format kontrolü
+ guard email.isValidEmail else {
+     completion(AuthResult(success: false, errorMessage: .invalidEmail))
+     return
+ }
+ */
+/*
+ 
+ print("Authentication error: \(error.localizedDescription)")
+ switch error.code {
+ case AuthErrorCode.invalidEmail.rawValue:
+     completion(AuthResult(success: false, errorMessage: .invalidEmail))
+ case AuthErrorCode.wrongPassword.rawValue:
+     completion(AuthResult(success: false, errorMessage: .wrongPassword))
+ case AuthErrorCode.userNotFound.rawValue:
+     completion(AuthResult(success: false, errorMessage: .userNotFound))
+ default:
+     completion(AuthResult(success: false, errorMessage: .authenticationError))
+ */
