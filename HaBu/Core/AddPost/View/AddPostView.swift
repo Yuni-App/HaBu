154c675c85bf3f4b1127dcf080121a9f1593f504
Yusuf Aydın <yusayd3825@gmail.com>
2024-02-22 15:50:19 +0300

    update addPost keyboard

--- HaBu/Core/AddPost/View/AddPostView.swift
+++ HaBu/Core/AddPost/View/AddPostView.swift
@@ -6,6 +6,34 @@
 //
 
 import SwiftUI
+import Combine
+
+extension View {
+    func endEditing() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+extension Notification {
+    var keyboardHeight: CGFloat {
+        (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect)?.height ?? 0
+    }
+}
+
+extension Publishers {
+    static var keyboardHeight: AnyPublisher<CGFloat, Never> {
+        let willShow = NotificationCenter.default.publisher(for: UIApplication.keyboardWillShowNotification)
+            .map { $0.keyboardHeight }
+
+        let willHide = NotificationCenter.default.publisher(for: UIApplication.keyboardWillHideNotification)
+            .map { _ in CGFloat(0) }
+
+        return MergeMany(willShow, willHide)
+            .eraseToAnyPublisher()
+    }
+}
+
+
 
 struct AddPostView: View {
     @Environment(\.dismiss) private var dismiss
@@ -14,50 +42,53 @@ struct AddPostView: View {
     init(){
         self._addPostVM = StateObject(wrappedValue: AddPostViewModel(postService: PostService()))
     }
-    
+    @State private var keyboardHeight: CGFloat = 0
     var body: some View {
-        VStack {
-            ZStack {
-                AddPostBackground()
-                VStack {
-                    AddPostAppBar(addpostVM: addPostVM, action: {dismiss()})
+        ZStack {
+            AddPostBackground()
+            VStack {
+                
+                AddPostAppBar(addpostVM: addPostVM, action: {dismiss()}).padding()
+                
+                HStack(alignment: .top) {
+                    UserTypeImage(showAlert: $addPostVM.showAlert,
+                                  alertType: $addPostVM.alertType,
+                                  radius: addPostVM.isAnonimType == .notSelected ? 7.0 :
+                                    addPostVM.isAnonimType == .anonymous ? 7.0 : 35.0,
+                                  image: addPostVM.isAnonimType == .notSelected ? .qUser :
+                                    addPostVM.isAnonimType == .anonymous ? .anonim : .mert)
+                    .padding(5)
                     
-                    HStack(alignment: .top) {
-                        UserTypeImage(showAlert: $addPostVM.showAlert,
-                                      alertType: $addPostVM.alertType,
-                                      radius: addPostVM.isAnonimType == .notSelected ? 7.0 :
-                                        addPostVM.isAnonimType == .anonymous ? 7.0 : 35.0,
-                                      image: addPostVM.isAnonimType == .notSelected ? .qUser :
-                                        addPostVM.isAnonimType == .anonymous ? .anonim : .mert)
-                        .padding(5)
-                        
-                        TextField("Ne düşünüyorsunuz ? ", text: $addPostVM.textContent, axis: .vertical)
-                            .lineLimit(9...)
-                            .background(Color.white)
-                            .cornerRadius(7)
-                            .padding(.vertical)
-                    }
-                    .background(Color.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 5, style: .circular))
-                    .shadow(color: Color.black.opacity(0.4), radius: 3, x: 0, y: 3)
-                    .padding()
-                    
-                    AddCategoryView(SelectedTags: $addPostVM.SelectedTags)
-                        .padding(.vertical, 20)
-                    
-                    VStack {
-                        AddPostToggle(isAnonimComment: $addPostVM.isAnonimComment)
-                        AddPostMedia(addpostVM: addPostVM)
-                    }
-                    .padding(.horizontal, 10)
+                    TextField("Ne düşünüyorsunuz ? ", text: $addPostVM.textContent, axis: .vertical)
+                        .lineLimit(9...)
+                        .background(Color.white)
+                        .cornerRadius(7)
+                        .padding(.vertical)
                 }
-              
-               
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 5, style: .circular))
+                .shadow(color: Color.black.opacity(0.4), radius: 3, x: 0, y: 3)
+                .padding()
+                
+                AddCategoryView(SelectedTags: $addPostVM.SelectedTags)
+                    .padding(.vertical, 20)
+
+                AddPostToggle(isAnonimComment: $addPostVM.isAnonimComment)
+                AddPostMedia(addpostVM: addPostVM).padding(.horizontal, 10)
             }
-            .navigationDestination(isPresented: $addPostVM.isShareSuccess, destination: {
-                TabbarView()
-            })
+            .onTapGesture {
+                self.endEditing()
+            }
+            
+            
+        }.onReceive(Publishers.keyboardHeight) { keyboardHeight in
+            self.keyboardHeight = keyboardHeight // Klavye yüksekliğini güncelliyoruz
         }
+        .padding(.top, keyboardHeight)
+        .navigationDestination(isPresented: $addPostVM.isShareSuccess, destination: {
+            TabbarView()
+        })
+        
         .sheet(isPresented: $addPostVM.isShowingImagePicker) {
             ImagePicker(selectedImages: $addPostVM.selectedImages)
         }
@@ -66,9 +97,7 @@ struct AddPostView: View {
         }
     }
     
-    #Preview {
-        AddPostView()
-    }
+    
     
     @ViewBuilder
     private func AddPostToggle(isAnonimComment : Binding<Bool>)->some View{
@@ -142,4 +171,8 @@ func AddPostBackground()-> some View {
         Const.primaryBackGroundColor
             .frame(height: Const.height * 7.6 / 10)
     }
+}
+
+#Preview {
+    AddPostView()
 }
