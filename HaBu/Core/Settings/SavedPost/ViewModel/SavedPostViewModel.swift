af9a0ebe205360e999608342922a1669111ca9c3
mertalp <mertalp010@gmail.com>
2024-02-25 19:03:22 +0300

    added saved post vm

--- HaBu/Core/Settings/SavedPost/ViewModel/SavedPostViewModel.swift
+++ HaBu/Core/Settings/SavedPost/ViewModel/SavedPostViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class SavedPostViewModel : ObservableObject {
+    @Published var savedPostList :[Post]  = []
+    static let shared = SavedPostViewModel()
+    private var savedPostService = SavedPostService.shared
+
+    func listenForSavedPost() async {
+        await savedPostService.listenForSavedPost { result in
+            switch result {
+            case .success(let posts):
+                DispatchQueue.main.async {
+                    posts.forEach {post in
+                        if !self.savedPostList.contains(post) {
+                            self.savedPostList.append(post)
+                        }
+                    }
+                }
+            case .failure(let error):
+                print("Bildirimler dinlenirken hata oluştu: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    
+    func stopListening(){
+        savedPostService.stopListening()
+    }
+}
