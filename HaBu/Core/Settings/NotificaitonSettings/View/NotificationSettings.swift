b702f62c2bc4c41d515146d66da0896bb129742c
mertalp <mertalp010@gmail.com>
2024-02-26 14:16:37 +0300

    bug fix in notification

---
+++ HaBu/Core/Settings/NotificaitonSettings/View/NotificationSettings.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationSettings.swift
+//  HaBu
+//
+//  Created by mert alp on 26.02.2024.
+//
+
+import SwiftUI
+
+struct NotificationSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationSettings()
+}
