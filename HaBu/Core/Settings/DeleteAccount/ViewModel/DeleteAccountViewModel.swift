53a17cbb0729f07dfe2f1169e3c59b6c5eb39d95
Yusuf Aydın <yusayd3825@gmail.com>
2024-03-11 13:45:47 +0300

    moved to archive when the user delete account

--- HaBu/Core/Settings/DeleteAccount/ViewModel/DeleteAccountViewModel.swift
+++ HaBu/Core/Settings/DeleteAccount/ViewModel/DeleteAccountViewModel.swift
@@ -19,6 +19,50 @@ class DeleteAccountViewModel: ObservableObject {
     @Published var showingAlert = false
     
     
+    func transferAndDeleteUserData(completion: @escaping (Bool, Error?) -> Void) {
+        guard let user = Auth.auth().currentUser else {
+            self.alertTitle = "Hata!"
+            self.alertMessage = "Kullanıcı oturum açmamış veya kullanılamıyor."
+            self.showingAlert = true
+            return
+        }
+        
+        let db = Firestore.firestore()
+        let originalDocument = db.collection("user").document(user.uid)
+        let backupDocument = db.collection("userArchive").document(user.uid)
+        
+        originalDocument.getDocument { (documentSnapshot, error) in
+            if let documentSnapshot = documentSnapshot, documentSnapshot.exists {
+                backupDocument.setData(documentSnapshot.data()!) { error in
+                    if let error = error {
+                        completion(false, error)
+                        self.alertTitle = "Hata!"
+                        self.alertMessage = "Veriler silinirken 1 hata oluştu."
+                        self.showingAlert = true
+                    } else {
+                        originalDocument.delete() { error in
+                            if let error = error {
+                                completion(false, error)
+                                self.alertTitle = "Hata!"
+                                self.alertMessage = "Veriler silinirken bir hata oluştu."
+                                self.showingAlert = true
+                            } else {
+                                completion(true, nil)
+                            }
+                        }
+                    }
+                }
+            } else {
+                completion(false, error ?? NSError(domain: "FirestoreError", code: -1, userInfo: nil))
+                self.alertTitle = "Hata!"
+                self.alertMessage = "Kullanıcı verileri bulunamadı."
+                self.showingAlert = true
+            }
+        }
+    }
+    
+    
+    
     func deleteAccount(completion: @escaping (Bool, Error?) -> Void) {
         guard let user = Auth.auth().currentUser else {
             // Kullanıcı oturum açmamış veya kullanılamamıyor hatasını ele al
@@ -35,19 +79,30 @@ class DeleteAccountViewModel: ObservableObject {
                 self.showingAlert = true
                 return
             } else {
-                user.delete { error in
-                    if let error = error {
+                // Önce kullanıcı verilerini yedekle ve sil
+                self.transferAndDeleteUserData { success, error in
+                    if success {
+                        // Kullanıcı verileri başarıyla yedeklendikten sonra kullanıcı hesabını sil
+                        user.delete { error in
+                            if let error = error {
+                                completion(false, error)
+                                self.alertTitle = "Hata!"
+                                self.alertMessage = "Hesap silme işlemi başarısız."
+                                self.showingAlert = true
+                                return
+                            } else {
+                                completion(true, nil)
+                                self.alertTitle = "Uyarı!"
+                                self.alertMessage = "Hesabınız silinmiştir."
+                                self.showingAlert = true
+                                return
+                            }
+                        }
+                    } else {
                         completion(false, error)
                         self.alertTitle = "Hata!"
-                        self.alertMessage = "Hesap silme işlemi başarısız."
+                        self.alertMessage = "Verilerin silinmesi sırasında bir hata oluştu."
                         self.showingAlert = true
-                        return
-                    } else {
-                        completion(true, nil)
-                        self.alertTitle = "Uyarı!"
-                        self.alertMessage = "Hesabınız silinmiştir."
-                        self.showingAlert = true
-                        return
                     }
                 }
             }
