ee2c6708d58c677da8291882699ed1eaf7cf6d77
mertalp <mertalp010@gmail.com>
2024-03-18 22:02:46 +0300

    added users lookup view model

--- HaBu/Core/Serach/new/UsersLookupViewModel.swift
+++ HaBu/Core/Serach/new/UsersLookupViewModel.swift
@@ -4,5 +4,26 @@
 //
 //  Created by mert alp on 18.03.2024.
 //
-
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class UsersLookupViewModel: ObservableObject {
+    @Published var queryResultUsers: [User] = []
+    
+    private let db = Firestore.firestore()
+    
+    func fetchUsers(with keyword: String) {
+        print("çalıştı")
+        db.collection("user").whereField("keywordsForLookup", arrayContains: keyword).getDocuments { querySnapshot, error in
+            guard let documents = querySnapshot?.documents, error == nil else {
+                print("No documents")
+                return
+            }
+            self.queryResultUsers = documents.compactMap { queryDocumentSnapshot in
+                try? queryDocumentSnapshot.data(as: User.self)
+            }
+        }
+    }
+}
